f15dd1c57c29a83285ca1bbf9ab29a90b08eabcb
Connor Ketcheson <connorketcheson01@gmail.com>
2024-03-17 16:10:57 +0900

    add gameplay, cards not being attributed to winner

--- Part3_OOP_Project.py
+++ Part3_OOP_Project.py
@@ -58,14 +58,14 @@ class Hand:
     cards from that hand. There should be an add and remove card method here.
     '''
     
-    def __init__(self, cards):
-        self.cards = cards
+    def __init__(self):
+        self.hand = []
         
     def add_card(self, card):
-        self.cards.append(card)
+        self.hand.append(card)
         
-    def remove_card(self):
-        return self.cards.pop(0)
+    def remove_cards(self):
+        self.hand.clear()
 
 class Player:
     """
@@ -73,20 +73,22 @@ class Player:
     class object. The Payer can then play cards and check if they still have cards.
     """
     
-    def __init__(self, name, hand):
+    def __init__(self, name, cards, hand):
         self.name = name
+        self.cards = cards
         self.hand = hand
 
     def play_card(self):
-        print("You have played {} .".format(self.hand[0]))
-        return(self.hand[0])
+        print("{name} has played {c}.".format(name = self.name, c = self.cards[0]))
+        self.hand.add_card(self.cards.pop(0))
+        return(self.hand.hand[0])
     
     def check_cards(self):
-        print("You have {} cards remaining.".format(len(self.hand)))
+        print("{name} has {rem} cards remaining.".format(name = self.name, rem = len(self.cards)))
     
-    # def play_war(self):
-    #     self.active_cards.append(self.hand[1, 2, 3, 4])
-    #     return(self.hand[1, 2, 3], self.hand[4])
+    def play_war(self):
+        self.hand.add_card(self.cards.pop(1, 2, 3, 4))
+        return(self.hand.hand[5])
 
 
 ######################
@@ -97,17 +99,58 @@ my_deck = Deck()
 my_deck.shuffle()
 my_deck.cut()
 
-player1 = Player(input('What is your name? '), my_deck.cards[0])
-player2 = Player("Computer", my_deck.cards[1])
-print("Hello {}.".format(player1.name))
-move = input("What would you like to do? Please type 'Play card' or 'Check cards'. ")
+player1 = Player(input('What is your name? '), my_deck.cards[0], Hand())
+player2 = Player("Computer", my_deck.cards[1], Hand())
+print("Hello {name}, you have {rem} cards remaining.".format(name = player1.name, rem = len(player1.cards)))
 
-if move.casefold() == "Play card":
-    player1.play_card
-elif move.casefold() == "Check cards":
-    player1.check_cards
-else:
-    move = input("Invalid selection, please type 'Play card' or 'Check cards'. ")
+def play_war():
+    move = input("What would you like to do? Please type 'Play card', 'Check cards', or 'Quit': ")
+    if move.casefold() == "play card":
+        print(player1.hand.hand)
+        my_card = player1.play_card()
+        pc_card = player2.play_card()
+        num1 = RANKS.index(my_card[0])
+        num2 = RANKS.index(pc_card[0])
+        if num1 > num2:
+            print("You won! Collect your cards!")
+            player1.cards.extend(player2.hand.hand)
+            player1.hand.remove_cards()
+            player2.hand.remove_cards()
+            play_war()
+        elif num2 > num1:
+            print("You lost!")
+            player2.cards.extend(player1.hand.hand)
+            player1.hand.remove_cards()
+            player2.hand.remove_cards()
+            play_war()
+        elif num1 == num2:
+            print("War! Play 4 more cards!")
+            war_card1 = player1.play_war()
+            war_card2 = player2.play_war()
+            war1 = RANKS.index(war_card1[0])
+            war2 = RANKS.index(war_card2[0])
+            if war1 > war2:
+                print("You won! Collect your cards!")
+                player1.cards.extend(player2.hand.hand)
+                player1.hand.remove_cards()
+                player2.hand.remove_cards()
+                play_war()
+            elif war2 > war1:
+                print("You lost!")
+                player2.cards.extend(player1.hand.hand)
+                player1.hand.remove_cards()
+                player2.hand.remove_cards()
+                play_war()
+        
+    elif move.casefold() == "check cards":
+        player1.check_cards()
+        play_war()
+    elif move.casefold() == "quit":
+        return   
+    else:
+        play_war()
+        
+play_war()
 
 
 # Use the 3 classes along with some logic to play a game of war!
